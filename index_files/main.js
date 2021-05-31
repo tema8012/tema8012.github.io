d913e42ff316df8d969915be7798dd7bd2941b59
tema8012 <85133496+tema8012@users.noreply.github.com>
2021-05-31 23:47:12 +0300

    j

---
+++ index_files/main.js
@@ -0,0 +1,2 @@
+$(document).ready(function(){$('a[href^="#buy"]').click(function(){var elementClick=$(this).attr("href");var destination=$(elementClick).offset().top;jQuery("html:not(:animated), body:not(:animated)").animate({scrollTop:destination},800);return false;})
+$('.reviews').slick({dots:false,infinite:true,speed:300,slidesToShow:3,slidesToScroll:1,responsive:[{breakpoint:976,settings:{slidesToShow:2,slidesToScroll:1}},{breakpoint:659,settings:{slidesToShow:1,slidesToScroll:1}}]});});
